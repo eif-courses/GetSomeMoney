fe3506b2620c9a06fbfc3bc4fb418f6c5270f6d4
Marius <m.gzegozevskis@gmail.com>
2020-01-06 00:32:33 +0200

    EXAMPLE FOR SHOP

---
+++ app/src/main/java/get/some/money/starter/Fragments/HomeFragment.kt
@@ -0,0 +1,26 @@
+package get.some.money.starter.Fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import get.some.money.starter.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_home, container, false)
+  }
+
+
+}
