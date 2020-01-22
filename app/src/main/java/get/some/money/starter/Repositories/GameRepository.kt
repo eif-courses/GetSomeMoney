b5dfb38c446e71c7bb6a8144672f95d1a9271a7c
Marius <m.gzegozevskis@gmail.com>
2020-01-23 01:00:30 +0200

    Alpha version (ADDED MAIN FUNCTIONS)

--- app/src/main/java/get/some/money/starter/Repositories/GameRepository.kt
+++ app/src/main/java/get/some/money/starter/Repositories/GameRepository.kt
@@ -2,9 +2,6 @@ package get.some.money.starter.Repositories
 
 import androidx.lifecycle.MutableLiveData
 import get.some.money.starter.Models.GameObject
-import get.some.money.starter.Models.Item
-import get.some.money.starter.R
-import kotlin.random.Random
 
 
 
@@ -17,19 +14,19 @@ class GameRepository private constructor(){
 
 
   fun loadGameObjects(): MutableLiveData<List<GameObject>> {
-    val temp = listOf(
-      GameObject("Star", R.drawable.star_02, Random.nextInt(300)),
-      GameObject("Star", R.drawable.star_03, Random.nextInt(300)),
-      GameObject("Star", R.drawable.star_04, Random.nextInt(140)),
-      GameObject("Star", R.drawable.star_05, Random.nextInt(300)),
-      GameObject("Star", R.drawable.star_06, Random.nextInt(200)),
-      GameObject("Star", R.drawable.star_02, Random.nextInt(500)),
-      GameObject("Star", R.drawable.star_03, Random.nextInt(900)),
-      GameObject("Star", R.drawable.star_04, Random.nextInt(300)),
-      GameObject("Star", R.drawable.star_05, Random.nextInt(400))
-    )
-    temp.shuffled()
-    gameObjects.value = temp
+//    val temp = listOf(
+//      GameObject("Star", R.drawable.star_02, Random.nextInt(300)),
+//      GameObject("Star", R.drawable.star_03, Random.nextInt(300)),
+//      GameObject("Star", R.drawable.star_04, Random.nextInt(140)),
+//      GameObject("Star", R.drawable.star_05, Random.nextInt(300)),
+//      GameObject("Star", R.drawable.star_06, Random.nextInt(200)),
+//      GameObject("Star", R.drawable.star_02, Random.nextInt(500)),
+//      GameObject("Star", R.drawable.star_03, Random.nextInt(900)),
+//      GameObject("Star", R.drawable.star_04, Random.nextInt(300)),
+//      GameObject("Star", R.drawable.star_05, Random.nextInt(400))
+//    )
+//    temp.shuffled()
+//    gameObjects.value = temp
     return gameObjects
   }
 }
