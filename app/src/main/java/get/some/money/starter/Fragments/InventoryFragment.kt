0298e245c837adb2d93f54e5d0cfe28cca5c0af9
Marius <m.gzegozevskis@gmail.com>
2020-01-24 20:18:41 +0200

    Alpha version v2 (ADDED MAIN FUNCTIONS)

--- app/src/main/java/get/some/money/starter/Fragments/InventoryFragment.kt
+++ app/src/main/java/get/some/money/starter/Fragments/InventoryFragment.kt
@@ -46,7 +46,8 @@ class InventoryFragment : Fragment(R.layout.fragment_inventory), InventoryListAd
   }
 
   override fun click_item(inventory: Inventory) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
   }
 }
 
--- app/src/main/java/get/some/money/starter/Fragments/ProfileFragment.kt
+++ app/src/main/java/get/some/money/starter/Fragments/ProfileFragment.kt
@@ -24,17 +24,36 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     userViewModel.getUser(FirebaseAuth.getInstance().currentUser!!.uid).observe(this, Observer {
 
-      coins_multiplier.append(it.multiplier.toString())
-      special_levels.append(it.specialLevels.toString())
-      game_tickets.append(it.gameTickets.toString())
-      feature_content.append(it.permanentDLC.toString())
+//      coins_multiplier.append(it.multiplier.toString())
+//      special_levels.append(it.specialLevels.toString())
+//      game_tickets.append(it.gameTickets.toString())
+//      feature_content.append(it.permanentDLC.toString())
       userNameProfileDetails.setText(it.name)
       }
     )
     saveUserProfileDetails.setOnClickListener {
       userViewModel.updateName(userNameProfileDetails.text.toString(), FirebaseAuth.getInstance().currentUser!!.uid)
 
-      Toast.makeText(context, "You name is updated", Toast.LENGTH_LONG).show()
+      Toast.makeText(context, getString(R.string.name_updated), Toast.LENGTH_LONG).show()
+
+      it.isClickable = false
+      it.visibility = View.GONE
+      userNameProfileDetails.visibility = View.GONE
+
+//      val timer = object : CountDownTimer(30000, 10000) {
+//      override fun onFinish() {
+//        it.visibility = View.VISIBLE
+//        it.isClickable = true
+////        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//      }
+//
+//      override fun onTick(millisUntilFinished: Long) {
+//  //      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//      }
+//
+//    }
+//
+//      timer.start()
     }
   }
 
