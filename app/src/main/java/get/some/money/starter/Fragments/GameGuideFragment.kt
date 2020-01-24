e681779de349ca85185e3e9d4aed4f106c835295
Marius <m.gzegozevskis@gmail.com>
2020-01-25 01:01:25 +0200

    Alpha version v2 (ADDED MAIN FUNCTIONS)

--- app/src/main/java/get/some/money/starter/Fragments/GameGuideFragment.kt
+++ app/src/main/java/get/some/money/starter/Fragments/GameGuideFragment.kt
@@ -3,5 +3,4 @@ package get.some.money.starter.Fragments
 
 import androidx.fragment.app.Fragment
 import get.some.money.starter.R
-
-class PrivacyPolicyFragment : Fragment(R.layout.fragment_privacy_policy)
+class GameGuideFragment : Fragment(R.layout.fragment_game_guide)
--- app/src/main/java/get/some/money/starter/Fragments/TermsAndConditionsFragment.kt
+++ app/src/main/java/get/some/money/starter/Fragments/TermsAndConditionsFragment.kt
@@ -4,4 +4,4 @@ package get.some.money.starter.Fragments
 import androidx.fragment.app.Fragment
 import get.some.money.starter.R
 
-class PrivacyPolicyFragment : Fragment(R.layout.fragment_privacy_policy)
+class TermsAndConditionsFragment : Fragment(R.layout.fragment_terms_and_conditions)
