333ed1b356bb785785c9cb807a80b03ee8091a80
Marius <m.gzegozevskis@gmail.com>
2020-01-28 23:26:00 +0200

    Alpha version v5 (FIXED BUGGS 50 more)

--- app/src/main/java/get/some/money/starter/Dialogs/RewardDialog.kt
+++ app/src/main/java/get/some/money/starter/Dialogs/RewardDialog.kt
@@ -11,7 +11,11 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
 import get.some.money.starter.R
+import get.some.money.starter.ViewModels.UserViewModel
 import kotlinx.android.synthetic.main.reward_dialog.*
 import kotlin.random.Random
 
@@ -19,7 +23,8 @@ import kotlin.random.Random
 class RewardDialog : DialogFragment(){
 
   lateinit var mediaPlayer: MediaPlayer
-  //lateinit var userModel: UserViewModel
+  lateinit var userViewModel: UserViewModel
+  private var gold = 0
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
@@ -31,10 +36,13 @@ class RewardDialog : DialogFragment(){
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    val totalEarned = Random.nextInt(50,100)
 
     mediaPlayer = MediaPlayer.create(view.context, R.raw.openchest)
+    userViewModel = ViewModelProviders.of(this)[UserViewModel::class.java]
 
+    userViewModel.getUser(FirebaseAuth.getInstance().currentUser!!.uid).observe(this, Observer {
+      gold = it.coins
+    })
     next_level_btn.isVisible = false
 
     next_level_btn.setOnClickListener {
@@ -48,7 +56,7 @@ class RewardDialog : DialogFragment(){
       it.isClickable = false
       next_level_btn.isVisible = true
       mediaPlayer.start()
-
+      val totalEarned = Random.nextInt(50,100)
       // Get the custom TOAST layout view.
       val toastView = getLayoutInflater().inflate(R.layout.toast_message, null);
       val toast = Toast(it.context)
@@ -62,6 +70,8 @@ class RewardDialog : DialogFragment(){
       toast.setGravity(Gravity.CENTER, 0,0);
       toast.show()
 
+
+      userViewModel.updateCoins(gold + totalEarned, FirebaseAuth.getInstance().currentUser!!.uid)
     }
 
 
