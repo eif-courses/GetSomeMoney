1bf65a3e85107e41d8f9bb89e0cfce62428731d1
Marius <m.gzegozevskis@gmail.com>
2020-02-04 01:58:23 +0200

    Alpha version v9 fixed issues added Splash screen

--- app/src/main/java/get/some/money/starter/Fragments/SiginInFragment.kt
+++ app/src/main/java/get/some/money/starter/Fragments/SiginInFragment.kt
@@ -1,13 +1,96 @@
 package get.some.money.starter.Fragments
 
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import get.some.money.starter.Models.User
 import get.some.money.starter.R
+import get.some.money.starter.ViewModels.UserViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class SiginInFragment : Fragment(R.layout.fragment_sigin_in) {
+  private val RC_SIGN_IN = 123
+  val userModel: UserViewModel by viewModels()
 
 
+  var uuid = FirebaseAuth.getInstance().currentUser?.uid
+  val providers = arrayListOf(
+    AuthUI.IdpConfig.EmailBuilder().build(),
+    AuthUI.IdpConfig.GoogleBuilder().build()
+  )
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    if (uuid == null) {
+      startActivityForResult(
+        AuthUI.getInstance()
+          .createSignInIntentBuilder()
+          .setAvailableProviders(providers)
+          .build(),
+        RC_SIGN_IN
+      )
+    } else {
+      val action = SiginInFragmentDirections.actionSiginInFragmentToHomeFragment()
+      findNavController().navigate(action)
+    }
+  }
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (requestCode == RC_SIGN_IN) {
+      val response = IdpResponse.fromResultIntent(data)
+      if (resultCode == Activity.RESULT_OK) {
+        // Successfully signed in
+
+        userModel.getUsers()
+          .observe(this, Observer<List<User>> { profiles ->
+
+            var exists = false
+
+            for (i in profiles.indices) {
+              if (profiles[i].uuid.equals(FirebaseAuth.getInstance().currentUser!!.uid)) {
+                //Toast.makeText(this, "USERIS EGZISTUOJA JUNGIAMES", Toast.LENGTH_LONG).show()
+                exists = true
+
+              } else {
+                println("Not exists!")
+              }
+            }
+            if (!exists) {
+              userModel.saveUser(
+                User(
+                  getString(R.string.startName),
+                  0,
+                  FirebaseAuth.getInstance().currentUser!!.uid,
+                  0
+                )
+              )
+            }
+
+          })
+
+        val action = SiginInFragmentDirections.actionSiginInFragmentToHomeFragment()
+        findNavController().navigate(action)
+
+      } else {
+        println(response?.error)
+        // Sign in failed. If response is null the user canceled the
+        // sign-in flow using the back button. Otherwise check
+        // response.getError().getErrorCode() and handle the error.
+        // ...
+      }
+    }
+  }
 }
--- app/src/main/java/get/some/money/starter/MainActivity.kt
+++ app/src/main/java/get/some/money/starter/MainActivity.kt
@@ -1,8 +1,6 @@
 package get.some.money.starter
 
-import android.app.Activity
 import android.app.AlertDialog
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -21,13 +19,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
-import get.some.money.starter.Models.User
 import get.some.money.starter.Util.Language
-import get.some.money.starter.ViewModels.LevelViewModel
 import get.some.money.starter.ViewModels.UserViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -35,64 +29,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(), AppBarConfiguration.OnNavigateUpListener {
 
   private lateinit var appBarConfiguration: AppBarConfiguration
-  private val RC_SIGN_IN = 123
-  val userModel: UserViewModel by viewModels()
-  val levelModel: LevelViewModel by viewModels()
-
-
-
+  private val userModel: UserViewModel by viewModels()
   var uuid = FirebaseAuth.getInstance().currentUser?.uid
-  val providers = arrayListOf(
-    AuthUI.IdpConfig.EmailBuilder().build(),
-    AuthUI.IdpConfig.GoogleBuilder().build()
-  )
-
-
   override fun onStart() {
     super.onStart()
 
-    // Create and launch sign-in intent
-
-//    val pref = SharedPreference(this)
-//
-//    pref.save("FIRST_RUN", true)
-//
-//    val isFirstRun = pref.getValueBoolien("FIRST_RUN", true)
-//
-//    if(isFirstRun) {
-//      startActivityForResult(
-//        AuthUI.getInstance()
-//          .createSignInIntentBuilder()
-//          .setAvailableProviders(providers)
-//          .build(),
-//        RC_SIGN_IN
-//      )
-//      pref.save("FIRST_RUN", false)
-
-    //pref.clearSharedPreference()
-    //}
-    if (uuid == null) {
-      startActivityForResult(
-        AuthUI.getInstance()
-          .createSignInIntentBuilder()
-          .setAvailableProviders(providers)
-          .build(),
-        RC_SIGN_IN
-      )
-
-    } else {
-
-
-      userModel.getUser(uuid!!).observe(this, Observer {
+    if (uuid != null) {
+      userModel.getUser(FirebaseAuth.getInstance().currentUser!!.uid).observe(this, Observer {
 
         if (it != null) {
           val navigationView = findViewById<NavigationView>(R.id.nav_view)
-          val header = navigationView.getHeaderView(0)
-          val profileName = header.findViewById<TextView>(R.id.Nickname)
-          val editName = header.findViewById<ImageView>(R.id.edit_text_header)
-          profileName.text = it.name
+          val header = navigationView?.getHeaderView(0)
+          val profileName = header?.findViewById<TextView>(R.id.Nickname)
+          val editName = header?.findViewById<ImageView>(R.id.edit_text_header)
+          profileName?.text = it.name
 
-          val _categoryName = EditText(this)
+          //val _categoryName = EditText(this)
 
 
           var title = "Change your name or nickname!"
@@ -115,7 +67,7 @@ class MainActivity : AppCompatActivity(), AppBarConfiguration.OnNavigateUpListen
           //  _categoryName.hint = hint
 
 
-          editName.setOnClickListener {
+          editName?.setOnClickListener {
 
 
             val dialogBuilder = AlertDialog.Builder(this)
@@ -125,8 +77,6 @@ class MainActivity : AppCompatActivity(), AppBarConfiguration.OnNavigateUpListen
             val name = dialogView.findViewById<EditText>(R.id.your_name_edittext)
             val cancel = dialogView.findViewById<Button>(R.id.cancel_edit_name)
             val accept = dialogView.findViewById<Button>(R.id.accept_your_name_edittext)
-
-
 
             name.hint = hint
 
@@ -148,45 +98,22 @@ class MainActivity : AppCompatActivity(), AppBarConfiguration.OnNavigateUpListen
                 name.text.toString(),
                 FirebaseAuth.getInstance().currentUser!!.uid
               )
-              Toast.makeText(applicationContext, success, Toast.LENGTH_LONG).show()
+              Toast.makeText(this, success, Toast.LENGTH_LONG).show()
               alertDialog.dismiss()
 
             }
-
-
-//            val _dialogBuilder = AlertDialog.Builder(this)
-//            _dialogBuilder.setMessage(message)
-//              .setTitle(title)
-//              .setPositiveButton(yes) {
-//                  dialog: DialogInterface?, which: Int ->
-//                if(_categoryName.text.length <=30){
-//                  //profileName.text = _categoryName.text
-//                  userModel.updateName(_categoryName.text.toString(), FirebaseAuth.getInstance().currentUser!!.uid)
-//
-//                  Toast.makeText(applicationContext, success, Toast.LENGTH_LONG).show()
-//                }else{
-//                  Toast.makeText(applicationContext, toolong, Toast.LENGTH_LONG).show()
-//                }
-//              }
-//              .setView(_categoryName)
-//            _dialogBuilder.setCancelable(false)
-//
-//            _dialogBuilder.show()
-
-
           }
         }
       })
+    }else{
 
     }
   }
 
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
-
-
 
 
     val toolbar = findViewById<Toolbar>(R.id.toolbar)
@@ -245,51 +172,6 @@ class MainActivity : AppCompatActivity(), AppBarConfiguration.OnNavigateUpListen
     }
     return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
         || super.onOptionsItemSelected(item)
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-
-    if (requestCode == RC_SIGN_IN) {
-      val response = IdpResponse.fromResultIntent(data)
-      if (resultCode == Activity.RESULT_OK) {
-        // Successfully signed in
-
-        userModel.getUsers()
-          .observe(this, Observer<List<User>> { profiles ->
-
-            var exists = false
-
-            for (i in profiles.indices) {
-              if (profiles[i].uuid.equals(FirebaseAuth.getInstance().currentUser!!.uid)) {
-                //Toast.makeText(this, "USERIS EGZISTUOJA JUNGIAMES", Toast.LENGTH_LONG).show()
-                exists = true
-
-              } else {
-                println("Not exists!")
-              }
-            }
-            if (!exists) {
-              userModel.saveUser(
-                User(
-                  "Puzzle solver",
-                  0,
-                  FirebaseAuth.getInstance().currentUser!!.uid,
-                  0
-                )
-              )
-            }
-
-          })
-
-      } else {
-        println(response?.error)
-        // Sign in failed. If response is null the user canceled the
-        // sign-in flow using the back button. Otherwise check
-        // response.getError().getErrorCode() and handle the error.
-        // ...
-      }
-    }
   }
 
 
